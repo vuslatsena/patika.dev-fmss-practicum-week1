89b00b0f8f5fffd347008af7e2f47c8d3bb9a07b
vuslatsena <vuslatsena23@gmail.com>
2023-04-01 00:15:50 +0300

    add comment line for functions

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-import readline from 'readline';
-import { getPosts, getUsers } from './getdata.js';
+const readline = require('readline');
+const { getUsers, getPosts } = require('./your-module');
 
+// Create a readline interface object
 const readlineInterface = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+// Prompt the user to enter a user ID and retrieve the user object with its posts
 readlineInterface.question('Please enter a user ID: ', async (userId) => {
   console.log(`User ID entered: ${userId}`);
+  // Check if the user entered a valid number
   if (isNaN(userId)) return console.error('The value entered is not a number!');
   try {
+    // Retrieve the user object with its posts
     const userWithPosts = { ...(await getUsers(userId)), posts: await getPosts(userId) };
     console.log(userWithPosts);
   } catch (error) {
+    // Handle errors
     console.error(error);
+  } finally {
+    // Close the readline interface
+    readlineInterface.close();
   }
-  readlineInterface.close();
 });
